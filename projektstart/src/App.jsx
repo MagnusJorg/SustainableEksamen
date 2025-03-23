2f2671c4afc7b851b8a436e6555eaf3f670e899b
MagnusJorg <eaa23mojoe@students.eaaa.dk>
2025-03-23 13:10:49 +0100

    s

---
+++ projektstart/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+import Headers from "./Headers";
+import Filter from "./filter";
+import Footer1 from "./Footer1";
+
+function App() {
+  return (
+    <>
+      <Headers />
+      <Filter />
+      <Footer1 />
+    </>
+  );
+}
+
+export default App;
---
+++ projektstart/src/Headers.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+export default function Headers() {
+    return (
+        <header>
+            <div className="logo">
+                {/* Use direct path if image is in public folder */}
+                <img src="/sundhedlogo.png" alt="Logo" />  
+            </div>
+            <nav>
+                <ul>
+                    <li>Træning</li>
+                    <li>Ernæring</li>
+                    <li>Tips</li>
+                    <li>Om os</li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
