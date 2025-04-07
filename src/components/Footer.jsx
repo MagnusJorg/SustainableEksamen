e1f55a02624a7a86730a6bce878fa2029031174b
MagnusJorg <eaa23mojoe@students.eaaa.dk>
2025-04-07 10:11:43 +0200

    f

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,22 @@
-import mystyle from "./Accordion.module.css";
-export default function Footer() {
+import { Link } from "react-router-dom";
+import mystyle from "./Footer.module.css";
 
+export default function Footer() {
     return (
-        <footer>
-            &copy; 2025 Morten Bonderup
+        <footer className={mystyle.footer}>
+            <section className={mystyle.footerKolonne}>
+                <h2>Det var et lille udpluk</h2>
+                <p><em>men jeg er kun en mail væk, hvis du vil vide mere!</em></p>
+            </section>
+
+            <section className={mystyle.kontakt}>
+                <Link to="/">FORSIDE</Link>
+                <Link to="/Hvt">HVAD VI TILBYDER</Link>
+                <Link to="/Svd">SÅDAN VIRKER DET</Link>
+                <Link to="/Oo">OM OS</Link>
+                <a href="mailto:Magnusjorgen@gmail.com">csm@kontakt.com</a>
+            </section>
         </footer>
-    )
+    );
 }
+
