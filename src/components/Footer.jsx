957d0cff56e1fa41265b0458cd09834c3ee3618d
MagnusJorg <eaa23mojoe@students.eaaa.dk>
2025-04-08 14:29:20 +0200

    f

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -16,7 +16,7 @@ export default function Footer() {
                 <Link to="/Hvt">HVAD VI TILBYDER</Link>
                 <Link to="/Svd">SÅDAN VIRKER DET</Link>
                 <Link to="/Oo">OM OS</Link>
-                <a href="mailto:Magnusjorgen@gmail.com">CsmKontakt@gmail.com</a>
+                <a href="mailto:Magnusjorgen@gmail.com">CSMKONTAKT@gmail.com</a>
             </section>
         </footer>
     );
